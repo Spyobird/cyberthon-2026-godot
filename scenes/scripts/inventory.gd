c001333ee2eb68e1b0335c1463ae645aa86e9501
Spyobird <elliotlimzy@gmail.com>
2026-02-28 13:21:55 +0800

    Add basic inventory implementation
    Also add initialisation to the player node

---
+++ scenes/scripts/inventory.gd
@@ -0,0 +1,30 @@
+class_name Inventory
+extends RefCounted
+
+const _VALID_ITEMS = ["magic wand", "door key"]
+const _MAX_SIZE = 4
+
+var _items: Array = []
+
+func load_items(items: Array[String]):
+	_items.clear()
+	for item in items:
+		if len(_items) >= _MAX_SIZE:
+			break
+		var valid_item = _validate_item(item)
+		if valid_item == null:
+			continue
+		_items.append(valid_item)
+
+
+func read_items(items: Array):
+	return _items
+
+func has_item(item) -> bool:
+	return item in _items
+
+func _validate_item(item: String):
+	# For now items are strings and handle validation here
+	if item.to_lower() in _VALID_ITEMS:
+		return item
+	return null
