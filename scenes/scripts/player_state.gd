6ff5f561f024a8325dbe6225fe67c4dd1f781768
kohwayne <kohwayne@live.com.sg>
2026-03-28 13:32:17 +0800

    Refactor: Update PlayerState to use Inventory class

--- scenes/scripts/player_state.gd
+++ scenes/scripts/player_state.gd
@@ -1,11 +1,11 @@
 class_name PlayerState
 extends Resource
 
-var inventory: Array[String]
+var inventory: Inventory
 var stats: Vector3i
 var moves: Array[String]
 
-func _init(inventory: Array[String], stats: Vector3i, moves: Array[String]):
-	self.inventory = inventory
+func _init(inventory_raw: Array[String], stats: Vector3i, moves_raw: Array[String]):
+	self.inventory = Inventory.new(inventory_raw)
 	self.stats = stats
-	self.moves = moves
+	self.moves = moves_raw
