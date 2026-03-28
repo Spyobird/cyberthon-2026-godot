9149c2d148adb8e47ba3aad30bd4c7104b31e8fe
kohwayne <kohwayne@live.com.sg>
2026-03-28 13:31:49 +0800

    Refactor: Expose helper functions to sync and update PlayerState. PlayerState should be accessed directly by any other scripts trying to player info

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,10 +1,15 @@
 class_name Player
 extends Node2D
 
+signal SYNC_PLAYER_START
+signal SYNC_PLAYER_SUCCESS
+signal SYNC_PLAYER_FAILED
+
 @onready var _movement_component = $TileBasedMovementComponent
 
 var _player_state_loader: PlayerStateLoader
-var _inventory: Inventory
+#var _inventory: Inventory
+var _current_player_state: PlayerState
 var _last_direction: Vector2
 
 func _ready() -> void:
@@ -13,16 +18,38 @@ func _ready() -> void:
 	
 	_movement_component.init(self)
 	
-	_inventory = Inventory.new()
-	_inventory.load_items(["magic wand"])
+	update_player_state(PlayerState.new(["magic wand"], Vector3i(100, 20, 20), ["fireball", "lightning"]))
+	print("Bootstrapped player!")
+	
+	#_inventory = Inventory.new()
+	#_inventory.load_items(["magic wand"])
 
 func _physics_process(delta) -> void:
 	_movement_component.process_movement(delta)
-		
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("test_api"):
-		var result = await _player_state_loader.load_player_state()
-		_inventory.load_items(result.inventory) # load player inventory
+
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("test_api"):
+		#var result = await _player_state_loader.load_player_state()
+		#update_player_state(result)
+		#_inventory.load_items(result.inventory) # load player inventory
+
+func get_player_state() -> PlayerState:
+	return _current_player_state
 
 func has_item(item: String) -> bool:
-	return _inventory.has_item(item)
+	return _current_player_state.inventory.has_item(item)
+	#return _inventory.has_item(item)/
+
+
+func update_player_state(new_player_state: PlayerState) -> void:
+	_current_player_state = new_player_state
+
+func sync_player_state() -> void:
+	SYNC_PLAYER_START.emit()
+	var result = await _player_state_loader.load_player_state()
+	if result == null:
+		SYNC_PLAYER_FAILED.emit()
+		return
+	update_player_state(result)
+	SYNC_PLAYER_SUCCESS.emit()
+	
