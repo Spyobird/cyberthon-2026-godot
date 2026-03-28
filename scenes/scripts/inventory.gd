f2ed68c73cfd212c41196478b7a862b7edc4aacd
kohwayne <kohwayne@live.com.sg>
2026-03-28 13:29:54 +0800

    Refactor: Allow Inventory to be instantiated with Array of strings

--- scenes/scripts/inventory.gd
+++ scenes/scripts/inventory.gd
@@ -6,6 +6,15 @@ const _MAX_SIZE = 4
 
 var _items: Array = []
 
+func _init(items: Array[String] = []):
+	for item in items:
+		if len(_items) >= _MAX_SIZE:
+			break
+		var valid_item = _validate_item(item)
+		if valid_item == null:
+			continue
+		_items.append(valid_item)
+	
 func load_items(items: Array[String]):
 	_items.clear()
 	for item in items:
@@ -17,7 +26,7 @@ func load_items(items: Array[String]):
 		_items.append(valid_item)
 
 
-func read_items(items: Array):
+func read_items():
 	return _items
 
 func has_item(item) -> bool:
@@ -27,4 +36,5 @@ func _validate_item(item: String):
 	# For now items are strings and handle validation here
 	if item.to_lower() in _VALID_ITEMS:
 		return item
+	print("%s is not a valid item name" % item)
 	return null
