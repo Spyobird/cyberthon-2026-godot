43b481a1991a7c3b6863edf169d487fb689c878b
kohwayne <kohwayne@live.com.sg>
2026-03-28 13:29:13 +0800

    feat(Menu): Add sub-menu for displaying player stats

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -5,36 +5,83 @@ extends Node2D
 
 var _camera: Camera2D
 var _movement_component: TileBasedMovementComponent
+var _player: Player
+var _stats_sub_menu: Node = null
 
-enum MenuState { NOTHING, MENU, ITEM_SCREEN }
+enum MenuState { NOTHING, MENU, ITEM_SCREEN, STATUS_SCREEN }
 var menu_state: MenuState = MenuState.NOTHING
 
-@onready var selected_option: int = 0
+enum MenuOptions {STATUS = 0, ITEM = 1, SYNC = 2, EXIT = 3}
+
+@onready var selected_option: int = MenuOptions.STATUS
 @onready var number_menu_options: int = ($Control/MenuBanner/TextContainer).get_child_count()
 
-# y position value of the cursor for the first
 const menu_start_offset_y = 7
-# increment to y, to select the next item
 const menu_next_item_offset_y = 14
 
 func calculate_arrow_position() -> void:
 	var new_arrow_position: int = menu_start_offset_y + (selected_option % number_menu_options) * menu_next_item_offset_y
 	select_arrow.set_position(Vector2(6.0, new_arrow_position))
-	
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	menu.visible = false
 	calculate_arrow_position()
 	_camera = get_viewport().get_camera_2d()
-	var player = get_tree().get_first_node_in_group("player_group")
-	if player:
-		_movement_component = player.get_node("TileBasedMovementComponent")
+	_player = get_tree().get_first_node_in_group("player_group")
+	if _player:
+		_movement_component = _player.get_node("TileBasedMovementComponent")
 
 func _process(_delta: float) -> void:
 	if _camera:
 		global_position = _camera.global_position
 
-	
+func _fade_to_black_then(callback: Callable) -> void:
+	var canvas = CanvasLayer.new()
+	var rect = ColorRect.new()
+	rect.color = Color.BLACK
+	rect.modulate = Color(1, 1, 1, 0)
+	rect.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	canvas.add_child(rect)
+	add_child(canvas)
+	var tween = create_tween()
+	tween.tween_property(rect, "modulate:a", 1.0, 0.5)
+	tween.tween_callback(callback)
+
+func _open_status_screen() -> void:
+	menu.visible = false
+	_stats_sub_menu = load("res://scenes/ui/stats_sub_menu.tscn").instantiate()
+	add_child(_stats_sub_menu)
+	menu_state = MenuState.STATUS_SCREEN
+
+func _open_item_screen() -> void:
+	_fade_to_black_then(func():
+		GameManager.scene_controller.overlay_2d_scene("res://scenes/ui/item_screen_menu.tscn")
+		menu.visible = false
+		menu_state = MenuState.ITEM_SCREEN
+	)
+
+func _sync_player_state() -> void:
+	if _player:
+		await _player.sync_player_state()
+		
+func _exit_menu() -> void:
+	menu.visible = false
+	menu_state = MenuState.NOTHING
+	GameManager.unlock_movement(&"menu")	
+
+func _handle_menu_select() -> void:
+	match selected_option:
+		MenuOptions.STATUS:
+			_open_status_screen()
+		MenuOptions.ITEM:
+			_open_item_screen()
+		MenuOptions.SYNC:
+			_sync_player_state()
+			print("Sync successful!")
+			_exit_menu()
+		MenuOptions.EXIT:
+			_exit_menu()
+			
 func _unhandled_input(event: InputEvent) -> void:
 	match menu_state:
 		MenuState.NOTHING:
@@ -43,36 +90,31 @@ func _unhandled_input(event: InputEvent) -> void:
 					return
 				if _movement_component and _movement_component.is_moving:
 					return
-				print("Menu is now visible!")
 				menu.visible = true
 				menu_state = MenuState.MENU
-				GameManager.is_player_movement_disabled = true
-		
+				GameManager.lock_movement(&"menu")
+
 		MenuState.MENU:
 			if event.is_action_pressed("toggle_menu"):
 				menu.visible = false
-				print("Menu is now hidden!")
 				menu_state = MenuState.NOTHING
-				GameManager.is_player_movement_disabled = false
+				GameManager.unlock_movement(&"menu")
 			elif event.is_action_pressed("ui_down"):
-				print("UI Down pressed")
-				selected_option += 1
+				selected_option = (selected_option + 1) % number_menu_options
 				calculate_arrow_position()
 			elif event.is_action_pressed("ui_up"):
-				print("UI up pressed")
-				if selected_option == 0:
-					selected_option = 5
-				else:
-					selected_option -= 1
+				selected_option = (selected_option - 1 + number_menu_options) % number_menu_options
 				calculate_arrow_position()
-			
+			elif event.is_action_pressed("use"):
+				_handle_menu_select()
 
-				
-				
-				
+		MenuState.STATUS_SCREEN:
+			if event.is_action_pressed("toggle_menu"):
+				if _stats_sub_menu:
+					_stats_sub_menu.queue_free()
+					_stats_sub_menu = null
+				menu.visible = true
+				menu_state = MenuState.MENU
+
 		MenuState.ITEM_SCREEN:
 			pass
-			
-
-		
-			
---
+++ scenes/ui/stats_sub_menu.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var hp_text: RichTextLabel = $Control/StatsMenu/TextContainer/HPText
+@onready var atk_text: RichTextLabel = $Control/StatsMenu/TextContainer/ATKText
+@onready var def_text: RichTextLabel = $Control/StatsMenu/TextContainer/DEFText
+
+var _player: Player
+
+func _ready() -> void:
+	_player = get_tree().get_first_node_in_group("player_group")
+	if _player:
+		_player.SYNC_PLAYER_SUCCESS.connect(_on_sync_success)
+		_update_stats(_player.get_player_state())
+
+func _update_stats(state: PlayerState) -> void:
+	hp_text.text = "HP: %d" % state.stats.x
+	atk_text.text = "ATK: %d" % state.stats.y
+	def_text.text = "DEF: %d" % state.stats.z
+
+func _on_sync_success() -> void:
+	_update_stats(_player.get_player_state())
