85fdc62015c9dde9de577c0f9efc4fc1be9eee3b
kohwayne <kohwayne@live.com.sg>
2026-04-05 18:58:15 +0800

    Fix: Listen for new signal names

--- scenes/ui/stats_sub_menu.gd
+++ scenes/ui/stats_sub_menu.gd
@@ -9,7 +9,7 @@ var _player: Player
 func _ready() -> void:
 	_player = get_tree().get_first_node_in_group("player_group")
 	if _player:
-		_player.SYNC_PLAYER_SUCCESS.connect(_on_sync_success)
+		_player.sync_player_completed.connect(_on_sync_success)
 		_update_stats(_player.get_player_state())
 
 func _update_stats(state: PlayerState) -> void:
