c3770d69fa51b00b319773aab4327d5b66664aaa
Spyobird <elliotlimzy@gmail.com>
2026-04-14 16:11:44 +0800

    Add hint page for stats hint

--- scenes/interactables/key.gd
+++ scenes/interactables/key.gd
@@ -7,4 +7,4 @@ func _ready() -> void:
 	_interactable_component.interacted.connect(_on_interacted)
 
 func _on_interacted(collider):
-	GameManager.create_message_popup("Key: [color=firebrick]ABCDABCD[/color]")
+	GameManager.create_message_popup("Key: [color=firebrick]ABCDEF[/color]")
