c5d9f9aefa82d5a10adaed78d77d0765c44ae2a7
DisarmingSnail <kohwayne@live.com.sg>
2026-04-14 08:47:04 +0800

    Feature: Add Item Sub-Menu + Refactoring (#27)
    * Fix: Item max size should be 3 according to specs
    
    * Feature: Add item sub-menu
    
    * Refactor:
    - Use CanvasLayer for menu to automatically anchor menu position to screenspace.
    - Menu and sub-menu will now communicate through signals instead

--- scenes/ui/stats_sub_menu.gd
+++ scenes/ui/stats_sub_menu.gd
@@ -1,21 +1,20 @@
-extends Node2D
+extends CanvasLayer
 
-@onready var hp_text: RichTextLabel = $Control/StatsMenu/TextContainer/HPText
-@onready var atk_text: RichTextLabel = $Control/StatsMenu/TextContainer/ATKText
-@onready var def_text: RichTextLabel = $Control/StatsMenu/TextContainer/DEFText
-
-var _player: Player
+@onready var hp_text: RichTextLabel = $StatsMenu/TextContainer/HPText
+@onready var atk_text: RichTextLabel = $StatsMenu/TextContainer/ATKText
+@onready var def_text: RichTextLabel = $StatsMenu/TextContainer/DEFText
 
 func _ready() -> void:
-	_player = get_tree().get_first_node_in_group("player_group")
-	if _player:
-		_player.sync_player_completed.connect(_on_sync_success)
-		_update_stats(_player.get_player_state())
+	visible = false
 
-func _update_stats(state: PlayerState) -> void:
-	hp_text.text = "HP: %d" % state.stats.x
-	atk_text.text = "ATK: %d" % state.stats.y
-	def_text.text = "DEF: %d" % state.stats.z
+func _on_status_menu_selected(hp: int, atk: int, def: int) -> void:
+	visible = true
+	_update_stats(hp, atk, def)
 
-func _on_sync_success() -> void:
-	_update_stats(_player.get_player_state())
+func _on_status_menu_closed() -> void:
+	visible = false
+
+func _update_stats(hp: int, atk: int, def: int) -> void:
+	hp_text.text = "HP: %d" % hp
+	atk_text.text = "ATK: %d" % atk
+	def_text.text = "DEF: %d" % def
