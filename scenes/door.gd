62c988ae14f0243c047d57353c41655c7003667c
kohwayne <kohwayne@live.com.sg>
2026-05-02 15:45:01 +0800

    feat(animations): Add door open sprite and simple transition animation

--- scenes/door.gd
+++ scenes/door.gd
@@ -7,7 +7,7 @@ const _DOOR_SPRITES = [
 ]
 
 @onready var _interactable_component = $InteractableComponent
-@onready var _door_sprite = $Sprites/DoorSprite
+@onready var _animation_player = $Sprites/AnimationPlayer
 var _is_open: bool = false
 
 # Called when the node enters the scene tree for the first time.
@@ -15,25 +15,26 @@ func _ready() -> void:
 	_interactable_component.is_collidable = true
 	_interactable_component.interacted.connect(_on_interact)
 	
-	_door_sprite.texture = _DOOR_SPRITES[0]
+	#_door_sprite.texture = _DOOR_SPRITES[0]
 
 func _on_interact(player):
 	if not player is Player:
 		return
 	if player.has_item("door key"):
 		var closed = GameManager.create_message_popup(
-			"Let's try using the [color=blue]door key[/color].",
-			"The door is now unlocked!"
+			["Let's try using the [color=blue]door key[/color].",
+			"The door is now unlocked!"]
 		)
 		await closed # just await the signal first
 		# open the door
+		_animation_player.play("door_opening")
 		_is_open = true
-		_interactable_component.is_collidable = false
 		_interactable_component.interacted.disconnect(_on_interact)
-		_door_sprite.texture = _DOOR_SPRITES[1]
+		#_interactable_component.is_collidable = false
+		#_door_sprite.texture = _DOOR_SPRITES[1]
 		
 	else:
 		GameManager.create_message_popup(
-			"Hmm... This door seems to be locked.",
-			"If I had a [color=blue]door key[/color], maybe I could unlock it..."
+			["Hmm... This door seems to be locked.",
+			"If I had a [color=blue]door key[/color], maybe I could unlock it..."]
 		)
