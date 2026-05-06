f160425cba604cc20bdef90f45a07254c082cd22
kohwayne <kohwayne@live.com.sg>
2026-05-06 21:50:22 +0800

    misc: Add enum to switch between different game levels

--- scenes/scene_controller.gd
+++ scenes/scene_controller.gd
@@ -4,6 +4,8 @@ extends Node
 @onready var world_2d = $World2D
 
 var _current_2d_scenes = []
+enum GAME_STAGE_TYPE { EMPEROR, RFID, RE }
+@export var game_stage = GAME_STAGE_TYPE.EMPEROR
 
 func _ready() -> void:
 	GameManager.scene_controller = self
@@ -13,8 +15,30 @@ func _ready() -> void:
 	var transition_controller: TransitionController = load("res://scenes/transition_controller.tscn").instantiate()
 	add_child(transition_controller)
 
-	overlay_2d_scene("res://scenes/levels/game.tscn")
-	#overlay_2d_scene("res://scenes/levels/emperor_stage.tscn")
+	var bgm_controller: BGMController = load("res://scenes/bgm_controller.tscn").instantiate()
+	add_child(bgm_controller)
+	
+	var bgm_track: AudioStream
+	var main_game_scene: String
+	
+	match game_stage:
+		GAME_STAGE_TYPE.RFID:
+			bgm_track = load("res://assets/audio/bg/normal.mp3")
+			main_game_scene = "res://scenes/levels/game_rfid.tscn"
+		GAME_STAGE_TYPE.EMPEROR:
+			bgm_track = load("res://assets/audio/bg/emperor.mp3")
+			main_game_scene = "res://scenes/levels/emperor_stage.tscn"
+		GAME_STAGE_TYPE.RE:
+			bgm_track = load("res://assets/audio/bg/normal.mp3")
+			main_game_scene = "res://scenes/levels/game_re.tscn"
+	
+	# Add track based on level
+	bgm_controller.overworld_track = bgm_track
+	bgm_controller.set_track(BGMController.TrackType.OVERWORLD)
+	bgm_controller.set_volume(0.6)
+	bgm_controller.play_track()
+
+	overlay_2d_scene(main_game_scene)
 	
 	
 func overlay_2d_scene(new_scene: String) -> Error:
