7d854b6a2f9fdc717768901a90e4031b12c1a566
kohwayne <kohwayne@live.com.sg>
2026-05-08 20:26:10 +0800

    fix: Last-mile patches and fixes

--- scenes/interactables/door_hint.gd
+++ scenes/interactables/door_hint.gd
@@ -3,11 +3,11 @@ extends Node2D
 
 const HINT_MESSAGES = [
 	"HOW TO USE P.A.W.S. (PROXMARK ARCANE WIRELESS SENTINEL)",
-	"Initialize the Arcane Terminal by typing [color=web_green][code]pm3[/code][/color] to bridge the connection.",
-	"To read the data, run the following commands in order:[ul][color=web_green][code]hf mf chk --1k --dump\nhf mf dump[/code][/color][/ul]",
+	"Initialize the Arcane Terminal by typing [bgcolor=black][color=web_green][code]pm3[/code][/color][/bgcolor] to bridge the connection.",
+	"To read the data, run the following commands in order:[ul][bgcolor=black][color=web_green][code]hf mf chk --1k --dump\nhf mf dump[/code][/color][/bgcolor][/ul]",
 	"The sector map is loading... Look there. Sector 1, Block 4. My [color=medium_blue]magic wand[/color] is currently stored there in ASCII format. That’s my active inventory slot.",
 	"For synthesis, 1st convert the string [color=medium_blue]door key[/color] into hexadecimal. Then write the hex data to the next available inventory block.",
-	"Run [color=web_green][code]hf mf wrbl --blk 5 -k FFFFFFFFFFFF -d <HEX_DATA>[/code][/color] to etch the key into the vault's memory.",
+	"Run [bgcolor=black][color=web_green][code]hf mf wrbl --blk 5 -k FFFFFFFFFFFF -d <HEX_DATA>[/code][/color][/bgcolor] to etch the key into the vault's memory.",
 	"Remember to sync the data from the card."
 ]
 
